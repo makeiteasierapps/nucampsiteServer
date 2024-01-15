748db99d9db30729df8791a41d2ba962260f5f2e
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-01-14 17:33:04 -0700

    Facebook Oauth

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const FacebookTokenStrategy = require('passport-facebook-token');
 const jwt = require('jsonwebtoken');
 const config = require('./config');
 
@@ -23,7 +24,7 @@ const options = {
 exports.jwtPassport = passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
         console.log('JWT payload:', jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
             if (err) {
                 return done(err, false);
             } else if (user) {
@@ -41,8 +42,41 @@ exports.verifyAdmin = (req, res, next) => {
     if (req.user.admin) {
         return next();
     } else {
-        const err = new Error('You are not authorized to perform this operation!');
+        const err = new Error(
+            'You are not authorized to perform this operation!'
+        );
         err.status = 403;
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ facebookId: profile.id }, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
